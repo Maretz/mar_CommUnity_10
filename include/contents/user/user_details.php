8a5cf214632f4264cee3abf090d7c31c554d69a0
Marcel Moritz <info@maretz.eu>
2017-02-07 21:42:30 +0100

    User Details

--- include/contents/user/user_details.php
+++ include/contents/user/user_details.php
@@ -17,6 +17,29 @@ if (db_num_rows($erg)) {
 	if ( file_exists($row['avatar'])) {
 		$avatar = '<img src="'.$row['avatar'].'" border="0">';
 	}
+else {
+		$avatar = '<img src="include/images/avatars/wurstegal.jpg" border="0">';
+}
+if ($row['opt_mail'] == 1) {
+  $usermail .= '<a rel="tooltip" title="Email scheiben" class="btn btn-primary" href="index.php?user-mail-'.$uid.'"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>';
+} else {
+  $usermail .= '';
+}
+if ($row['opt_pm'] == 1) {
+  $userpm .= '<a rel="tooltip" title="PM scheiben" class="btn btn-primary" href="index.php?forum-privmsg-new=0&amp;empfid='.$uid.'"><i class="fa fa-comments-o" aria-hidden="true"></i></a>';
+} else {
+  $userpm .= '';
+}
+if ($row['status'] == 1) {
+  $userstatus .= '<button rel="tooltip" title="'.$row['name'].' ist aktiv" type="button" class="btn btn-success"><i class="fa fa-battery-full" aria-hidden="true"></i></button>';
+} else {
+  $userstatus .= '<button rel="tooltip" title="'.$row['name'].' ist inaktiv" type="button" class="btn btn-danger"><i class="fa fa-battery-empty" aria-hidden="true"></i></button>';
+}
+if ($row['staat']) {
+  $userstaat .= '<img class="userflags" src="include/images/flags/'.$row['staat'].'">';
+} else {
+  $userstaat .= '';
+}
 
 	$regsek = mktime ( 0,0,0, date('m'), date('d'), date('Y') )  - $row['regist'];
 	$regday = round($regsek / 86400);
@@ -30,8 +53,12 @@ if (db_num_rows($erg)) {
 		'postpday' => $postpday,
 		'RANG'    => userrang ($row['posts'],$uid),
 		'AVATA'   => $avatar,
-	);
-
+    'WWW'   => '<a rel="tooltip" title="Webseite" class="btn btn-primary" href="'.$row['homepage'].'" target="_blank"><i class="fa fa-globe" aria-hidden="true"></i></a>',
+    'USERMAIL'   => $usermail,
+    'USERPM'   => $userpm,
+    'STAAT'   => $userstaat,
+    'STATUS'   => $userstatus,
+);
 
 	$title = $allgAr['title'].' :: Users :: Details von '.$row['name'];
 	$hmenu  = $extented_forum_menu.'<a class="smalfont" href="?user">Users</a><b> &raquo; </b> Details von '.$row['name'].$extented_forum_menu_sufix;
@@ -52,7 +79,7 @@ if (db_num_rows($erg)) {
 	$design = new design ( $title , $hmenu, 1);
 	$design->header();
 
-	echo 'Der Benutzer wurde nicht gefunden bzw. die Seite wurde nicht richtig aufgerufen.<br />';
+	echo '<div class="alert alert-warning" role="alert">Der Benutzer wurde nicht gefunden bzw. die Seite wurde nicht richtig aufgerufen.</div>';
 }
 
 $design->footer();
